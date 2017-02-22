a78ad1f3acdbb049989436a824bfd4363d586df0
JosephChow <hit_zp@126.com>
2017-02-22 17:18:58 +0800

    init Phabricator_Chinese Proj

---
+++ PhutilCNChineseLocale.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The default Chinese locale.
+ */
+final class PhutilCNChineseLocale extends PhutilLocale {
+
+  public function getLocaleCode() {
+    return 'zh_CN';
+  }
+
+  public function getLocaleName() {
+    return pht('简体中文 (zh_CN)');
+  }
+
+}
